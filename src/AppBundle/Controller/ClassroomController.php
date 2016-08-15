364e0d1c70686a05de772b689b29141ad7368643
AselaDarshan <aseladarshan@gmail.com>
2016-08-15 19:06:40 +0530

    Add Student and Teacher Classrooms

--- src/AppBundle/Controller/ClassroomController.php
+++ src/AppBundle/Controller/ClassroomController.php
@@ -18,9 +18,26 @@ class ClassroomController extends Controller
     /**
      * @Route("/classroom/student", name="studentClassroom")
      */
-    public function indexAction(Request $request)
-{
-    // replace this example code with whatever you need
-    return $this->render('@App/classroom/studentClassroom.html.twig');
-}
+    public function getStudentClassroomAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('@App/classroom/studentClassroom.html.twig');
+    }
+
+    /**
+     * @Route("/classroom/teacher/video", name="teacherVideo")
+     */
+    public function getTeacherVideoAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('@App/classroom/teacherVideo.html.twig');
+    }
+    /**
+     * @Route("/classroom/teacher/whiteboard", name="teacherWhiteboard")
+     */
+    public function getTeacherWhiteboardAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('@App/classroom/teacherWhiteboard.html.twig');
+    }
 }
